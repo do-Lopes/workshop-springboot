adceb673c23289f213d038fd5410aeaa93846fd1
Leonardo Lopes <leo2006lopes@gmail.com>
2024-02-20 11:10:09 -0300

    H2 database, test profile, JPA

--- src/main/java/com/shopservice/course/entities/User.java
+++ src/main/java/com/shopservice/course/entities/User.java
@@ -1,7 +1,13 @@
 package com.shopservice.course.entities;
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name= "tb_user")
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String  email;
