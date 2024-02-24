0fe5922b6b79cdbd4421d748c3d415863bf9664c
Leonardo Lopes <leo2006lopes@gmail.com>
2024-02-24 10:41:15 -0300

    Payment, Subtotal & Total methods, User CRUD and Exception handling

--- src/main/java/com/shopservice/course/entities/Order.java
+++ src/main/java/com/shopservice/course/entities/Order.java
@@ -27,6 +27,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
@@ -71,10 +74,25 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     public Set<OrderItem> getItems() {
         return items;
     }
 
+    public Double getTotal() {
+        double sum = 0;
+        for (OrderItem x : items) {
+            sum = sum + x.getSubTotal();
+        }
+        return sum;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
